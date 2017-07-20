2617bf4f11a537a50b6b90128ba4ef7a0b034be2
djdrysdale <david.drysdale@gmail.com>
2017-07-20 18:07:19 +0000

    Add flash message for correct or incorrect answer

--- routes/play.js
+++ routes/play.js
@@ -1,6 +1,8 @@
 var     express             = require("express"),
         router              = express.Router(),
         Question            = require("../models/question.js");
+        
+
 
 
 
@@ -15,5 +17,33 @@ router.get("/", function(req,res){
         }
     });
 });
+
+router.post("/", function(req,res){
+    Question.findById(req.body.questionID, function(err, foundQuestion){
+        if(err) {
+            req.flash("error",err);
+        } else {
+            if(foundQuestion.answer.indexOf(req.body.response) > -1){
+                console.log("Correct answer is " + foundQuestion.answer + " and your answer was " + req.body.response);
+                req.flash("success", "That is the correct answer!");
+                res.redirect("/play" );
+            } else {
+                console.log("Correct answer is " + foundQuestion.answer + " and your response was " + req.body.response);
+                req.flash("error", "That response was incorrect.");
+                res.redirect("/play" );
+            }
+        }
+    });
+});
+
+
+var getRandomQuestion = function(){
+    Question.findOneRandom(function(err, randomQuestion){
+        if(!err){
+            console.log(randomQuestion.question);
+        }
+    });
+};
+
 
 module.exports = router;
